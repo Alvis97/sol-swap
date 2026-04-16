dfac8e881884dd684109c51b5e26ea8a82ca582e
Alvis97 <101591973+Alvis97@users.noreply.github.com>
2026-04-16 12:38:35 +0100

    add wallet provider and wallet button

---
+++ src/app/components/providers.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { clusterApiUrl } from '@solana/web3.js'
+import React, { ReactNode, useMemo } from 'react'
+
+export default function Providers({ children} : { children: ReactNode }) {
+    const network = WalletAdapterNetwork.Mainnet
+    const endpoint = useMemo(() => clusterApiUrl(network), [network])
+    const wallets = useMemo(() => [new PhantomWalletAdapter()], [network])
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets}>
+            <WalletModalProvider>
+               <WalletMultiButton>
+                 {children}
+               </WalletMultiButton>
+            </WalletModalProvider>
+        </WalletProvider>
+    </ConnectionProvider>
+  )
+}
